3329f9a991bf84ebc11b0d85c4d2a296e0b03a1f
jbv2e <j4petv2@gmail.com>
2023-10-13 17:29:02 +0900

    input id

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -11,7 +11,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm></MealItemForm>
+        <MealItemForm id={props.id}></MealItemForm>
       </div>
     </li>
   )
